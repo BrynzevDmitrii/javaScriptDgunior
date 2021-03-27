9631ddfade99d56643a99328c5e85c00007957f0
BrynzevDmitrii <elf_happy@mail.ru>
2021-03-27 13:10:28 +0300

    первый урок по js

---
+++ java.js
@@ -0,0 +1,17 @@
+var tc = prompt('введите температуру в градусах Цельсия');
+var tf = ((9/5)*tc + 32 );
+alert ('температура в Форенгейтах: ' + tf);
+
+var ntame = '"Василий"';
+var admin = ntame ;
+alert ('admin = '+ admin) ;
+
+var namber = 1000;
+var string = '108';
+var sum = namber + string;
+alert ('результат : ' + sum); 
+
+
+
+
+
